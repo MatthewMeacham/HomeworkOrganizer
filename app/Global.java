52a751ed6d47b19a95b23f44304335adbb5c915d
MatthewMeacham <mmeacham6@gmail.com>
2014-11-01 13:57:17 -0500

    Created simple UI
    Began creating the webpage user interface, currently only a table that
    displays the data in the database.

---
+++ app/Global.java
@@ -0,0 +1,19 @@
+import play.*;
+import play.libs.*;
+import com.avaje.ebean.Ebean;
+import models.*;
+import java.util.*;
+
+public class Global extends GlobalSettings {
+
+	//this will change all the data in the database to the data contained in the test-data.yml file
+	private boolean overwriteAllDataInDatabase = false;
+	
+	@Override
+	public void onStart(Application app) {
+		if(Student.find.findRowCount() == 0 || overwriteAllDataInDatabase){
+			Ebean.save((List) Yaml.load("test-data.yml"));
+		}
+	}
+	
+}
