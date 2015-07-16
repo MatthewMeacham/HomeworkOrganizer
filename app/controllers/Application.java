b674e5541b9d61666d7166208a9640bee5843793
MatthewMeacham <mmeacham6@gmail.com>
2015-07-16 13:58:37 -0500

    Remove duplicate privacyPolicy methods in Application

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -77,11 +77,6 @@ public class Application extends Controller {
 		return ok(views.html.privacyPolicy.render());
 	}
 
-	// Directs the user to the privacy policy page
-	public Result privacyPolicy() {
-		return ok(views.html.privacyPolicy.render());
-	}
-
 	// Performs the email sending operation and then redirects back to the index
 	public Result contactUs() {
 		Form<ContactUs> filledForm = contactUsForm.bindFromRequest();
