1600f3600bbfc466f40b08249aa3e078c8c3a211
andrewgarrison <Andrew@Andrews-MacBook-Pro.local>
2015-08-08 22:52:37 -0500

    Teacher current date, Unauthorized styling
    Added the script in teacher profile so when they add an assignment the
    current date is set.
    Styled the unauthorized page

--- app/assets/scripts/teacherProfile.js
+++ app/assets/scripts/teacherProfile.js
@@ -221,3 +221,25 @@ $(".pull").bind('click', function() {
 	$('#toggle').toggleClass("on");
 	return false;
 });
+
+$(".pull").bind('click', function() {
+	$('#toggle').toggleClass("on");
+	return false;
+});
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
+
+if (dd<10) {
+	dd = '0' + dd;
+}
+if (mm<10) {
+	mm = '0' + mm;
+}
+today = mm+'/'+dd+'/'+yyyy;
+
+document.getElementById("monthassignment").value = mm;
+document.getElementById("daysassignment").value = dd;
+document.getElementById("yearassignment").value = yyyy;
